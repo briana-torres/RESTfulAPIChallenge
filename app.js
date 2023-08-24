77d3d944c969b608c5c781a2865d895eaa27c7f9
Briana Torres <briana@MacBook-Pro.local>
2023-08-24 09:13:16 -0400

    redid endpoints w MongoDB

--- app.js
+++ app.js
@@ -1,80 +1,76 @@
 const express = require('express');
-const { Client } = require('pg');
-require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Restaurant = require('./restaurantSchema');
+
+const dbURI = 'mongodb+srv://briana:RyOAI7FA5pJoe4bB@generate.nj9uh1g.mongodb.net/generate_sample_data';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+// Middleware for parsing JSON
+app.use(bodyParser.json());
 
-app.get('/restaurants/:n', async (req, res) => {
+app.get('/top/:n', async (req, res) => {
     const n = parseInt(req.params.n, 10);
 
     try {
-        await client.connect();
-
-        const query = `
-            SELECT name, AVG((grade->>'score')::FLOAT) AS avgScore
-            FROM restaurants,
-            LATERAL jsonb_array_elements(grades) AS grade
-            GROUP BY name
-            ORDER BY avgScore DESC
-            LIMIT $1
-        `;
-
-        const { rows } = await client.query(query, [n]);
-
-        const formattedData = rows.map((row) => ({
-            name: row.name,
-            avgScore: parseFloat(row.avgscore.toFixed(2)),
-        }));
-
-        res.json(formattedData);
+        let restaurants = await Restaurant.aggregate([
+            {
+                $addFields: {
+                    numberOfGrades: { $size: '$grades' },
+                    averageScore: { $avg: '$grades.score' }, 
+                },
+            },
+            {
+                $match: { 'numberOfGrades': { $gte: 5 } } 
+            },
+            {
+                $sort: { averageScore: -1 },
+            },
+            {
+                $limit: n,
+            },
+            ])
+            restaurants = restaurants.map(restaurant => ({
+                name: restaurant.name,
+                avgScore: restaurant.averageScore,
+            }));
+            res.status(200).json(restaurants);
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        await client.end();
     }
 });
 
-app.put('/restaurants/:name', async (req, res) => {
-    const { name } = req.params;
+app.put('/update/:restaurant_name', async (req, res) => {
+    const restaurantName = req.params.restaurant_name;
     const { grade } = req.body;
+    console.log(grade, restaurantName);
 
     try {
-        await client.connect();
+        const restaurant = await Restaurant.findOne({ name: restaurantName });
 
-        const updateQuery = `
-        UPDATE restaurants
-        SET grades = jsonb_set(grades, '{-1}', ('{"date": {"$date": ' || extract(epoch from current_timestamp) * 1000 || '}, "grade": "' || $1 || '", "score": ' || CAST($2 AS TEXT) || '}')::jsonb)
-        WHERE name = $3
-        RETURNING *
-    `;
-
-        const { rows } = await client.query(updateQuery, [grade, 0, name]);
-
-        if (rows.length === 0) {
+        if (!restaurant) {
             return res.status(404).json({ error: 'Restaurant not found' });
         }
 
-        res.json(rows[0]);
+        restaurant.grades[0].grade = grade;
+
+        await restaurant.save();
+
+        res.status(200).json({ message: 'Grade updated successfully' });
     } catch (error) {
         console.error('Error updating grade:', error);
         res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        await client.end();
     }
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
 
 
---
+++ restaurantSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+    address: {
+        building: String,
+        coord: [Number],
+        street: String,
+        zipcode: String,
+    },
+    borough: String,
+    cuisine: String,
+    grades: [
+        {
+            date: { type: Date },
+            grade: String,
+            score: Number,
+        },
+    ],
+    name: String,
+    restaurant_id: String,
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = Restaurant;
